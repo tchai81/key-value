a02d3d95ee9d7c016be5a5d176ebfa8b06bf621e
Chai Tze Chye <hologram38@gmail.com>
2020-09-27 12:26:05 +0800

    Assigned fillable attribute for models

--- app/Models/Key.php
+++ app/Models/Key.php
@@ -9,6 +9,14 @@ class Key extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+
     public function values() {
         return $this->hasMany('App\Models\KeyValue');
     }
--- app/Models/KeyValue.php
+++ app/Models/KeyValue.php
@@ -9,6 +9,13 @@ class KeyValue extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['value'];
+
     public function key() {
         return $this->belongsTo('App\Models\Key');
     }
